6cedc5be7cfb919de165fe405b10e61132c6fd19
dikshika <dikshika.gupta@teradata.com>
2021-12-06 13:28:19 +0530

    fix2

--- src/main/java/SpringCloudConfigServer.java
+++ src/main/java/SpringCloudConfigServer.java
@@ -1,9 +1,7 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 class SpringCloudConfigSever {
 
     public static void main (String[] args){
